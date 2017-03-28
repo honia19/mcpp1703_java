bea0e0548a1ac164c5cf2b4539b0ef8b3ab69c4d
honia19 <honchenko@gmail.com>
2017-03-28 20:42:53 +0300

    Домашние задание, из фора в вайл, ду вайл

---
+++ Task1/src/Java_HW/Lessonclass.java
@@ -0,0 +1,64 @@
+package Java_HW;
+
+public class Lessonclass {
+
+	public static void main(String[] args) {
+//		for (int i=1; i<=10;i++)
+//		{
+//			System.out.print(i+" ");
+//		}
+//		System.out.println();
+//		for(int j=20;j<=30;j++)
+//		{
+//			System.out.print(j+" ");
+//		}
+//		System.out.println();
+//		for(int a=20;a>=-10;a--)
+//		{
+//			System.out.print(a+" ");
+//		}
+//		System.out.println();
+//		for(int z=1;z<=100;z++)
+//		{
+//			if (z%5==0)
+//			System.out.print(z+" ");
+//		}
+//		System.out.println();
+//		for(int d=1;d<=100;d++)
+//		{
+//			if (d%3==0 && d%7==0)
+//			{
+//			System.out.print(d+" ");
+//			}
+//			
+//		}
+//		int c=0;
+//		while( c!=10)
+//		{
+//			c++;
+//			System.out.print(c+" ");
+//		}
+		
+		for(char a='A';a<='Z';a++)
+		{
+			System.out.print(a+" ");
+		}
+		System.out.println();
+		for(char a='À';a<='ß';a++)
+		{
+			System.out.print(a+" ");
+		}
+		System.out.println();
+		for(char a='ÿ';a>='à';a--)
+		{
+			System.out.print(a+" ");
+		}
+		System.out.println();
+		for(char a='0';a<='9';a++)
+		{
+			System.out.print(a+" ");
+		}
+
+	}
+
+}
